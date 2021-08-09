a143be9c274cf1dc9415a47eabc9a70481d30af8
Om Parkash <omparkashladhani@gmail.com>
2021-08-09 17:08:22 +0500

    feat:#4 Student Grade prediction with Keras Model

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import numpy as np
 from src import Polynomial
+from src.keras_model import KerasModel
+import pandas as pd
 
 if __name__ == '__main__':
-    coeffs = np.array([1,0,0])
+    coeffs = np.array([1, 0, 0])
     polynom = Polynomial(coeffs)
     print(polynom.evaluate(3))
     print(polynom.roots())
+data = pd.read_csv(
+    'https://raw.githubusercontent.com/Parkash9967/Test/08b6957aa8c693b60a66d741f2e1704ae73e536b/student-mat'
+    '.csv')
+data.drop(["school", "address", "sex", "famsize", "Pstatus", "Mjob", "Fjob",
+           "reason", "guardian",
+           "schoolsup", "famsup", "paid", "activities",
+           "nursery", "higher", "internet", "romantic"],
+          axis=1)
+k = KerasModel(
+    data[['age', 'Medu', 'Fedu', 'famrel', 'famrel', 'goout', 'Dalc', 'Walc', 'health', 'absences', 'G2', 'G1']],
+    data['G3'])
+
+print(k.grade_prediction_using_keras())
+# print(k.save_results())
---
+++ tests/keras_model.py
@@ -0,0 +1,29 @@
+"""
+Unit test module for Keras_model
+"""
+import unittest
+
+import keras.callbacks
+import pandas as pd
+from src.keras_model import KerasModel
+
+
+class MyTestCase(unittest.TestCase):
+    """
+    Unit test class for Keras_model
+    """
+    def setUp(self):
+        data = pd.read_csv(
+            'https://raw.githubusercontent.com/Parkash9967/Test/08b6957aa8c693b60a66d741f2e1704ae73e536b/student-mat'
+            '.csv')
+        self.data = KerasModel(
+            data[['age', 'Medu', 'Fedu', 'famrel', 'famrel', 'goout', 'Dalc', 'Walc', 'health', 'absences', 'G2',
+                  'G1']],
+            data['G3'])
+
+    def test_grade_prediction_using_keras(self):
+        """
+           here we check if it is an instance of Keras Library
+        """
+        get_results = self.data.grade_prediction_using_keras()
+        self.assertIsInstance(get_results, keras.callbacks.History)
